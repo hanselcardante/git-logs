93791bc7c293e7b159a0ad7206161765503b734b
Hansel Cardante <hansel.cardante@chromedia.com>
2018-10-09 11:09:21 +0800

    Create config files

---
+++ src/public/config.php
@@ -0,0 +1,3 @@
+<?php
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
--- src/public/index.php
+++ src/public/index.php
@@ -3,8 +3,9 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+require './config.php';
 
-$app = new \Slim\App;
+$app = new \Slim\App(['settings' => $config]);
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
